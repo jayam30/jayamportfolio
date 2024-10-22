f87afd900dc811abfba6ccdb2e812d2034633cf2
jayam30 <panditjayam@gmail.com>
2024-10-23 01:19:32 +0530

    updated project link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href=""
+                href="https://github.com/jayam30/Portfolio-master"
                 target="_blank"
                 className="fork-btn-inner"
               >
